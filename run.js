f5c674458beb46df4fcef688adec92f3544554d0
Dave Miller <dwmiller@umich.edu>
2011-07-17 01:51:45 -0700

    Fix file paths

--- run.js
+++ run.js
@@ -8,26 +8,22 @@ if (hash) {
   d.out(loop);
   d.out(g.score(loop));
 } else {
-  // 
-  // function mate_concat(loop1, loop2) {
-  //   return loop1.concat(loop2);
-  // }
-  // 
-  // d.out('breed');
-  // var kids = g.breed([1,2,5,7,14],[2,3,7,5,22,1]);
-  // d.out(kids.length);
-  // for(var i=0;i<kids.length;i++) {
-  //   d.out(kids[i]);
-  // }
+
+  d.out('breed');
+  var kids = g.breed([1,2,5,7,14],[2,3,7,5,22,1]);
+  d.out(kids.length);
+  for(var i=0;i<kids.length;i++) {
+    d.out(kids[i]);
+  }
 
   // while(true) {
-    l = g.generate_loop();
-    d.draw(l);
-    s = g.score(l);
+    // l = g.generate_loop();
+    // d.draw(l);
+    // s = g.score(l);
     // if (s.points > 50) {
-    d.draw(l);
-    d.out(l);
-    d.out(s);
+    // d.draw(l);
+    // d.out(l);
+    // d.out(s);
   //     break;
   //   }
   // }
